5a617f6de1250b9ec37c63389c9661ba8adf1d60
joaopaludo <joaopgpaludo@gmail.com>
2024-05-19 14:53:56 -0300

    Calendário comentado pois ainda está dando erro

--- app/calendario/page.tsx
+++ app/calendario/page.tsx
@@ -7,3 +7,36 @@ const Page: React.FC = () => {
 };
 
 export default Page;
+
+// import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+// import format from "date-fns/format";
+// import parse from "date-fns/parse";
+// import startOfWeek from "date-fns/startOfWeek";
+// import getDay from "date-fns/getDay";
+// import enUS from "date-fns/locale/en-US";
+
+// const locales = {
+//     "en-US": enUS,
+// };
+
+// const localizer = dateFnsLocalizer({
+//     format,
+//     parse,
+//     startOfWeek,
+//     getDay,
+//     locales,
+// });
+
+// const MyCalendar = () => (
+//     <div>
+//         <Calendar
+//             localizer={localizer}
+//             events={[]}
+//             startAccessor="start"
+//             endAccessor="end"
+//             style={{ height: 500 }}
+//         />
+//     </div>
+// );
+
+// export default MyCalendar;
