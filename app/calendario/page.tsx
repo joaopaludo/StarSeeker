094e4a7b8c008be244991248df081e9b91a67de4
joaopaludo <joaopgpaludo@gmail.com>
2024-05-30 21:07:08 -0300

    Calendar initial config

--- app/calendario/page.tsx
+++ app/calendario/page.tsx
@@ -3,19 +3,43 @@
 import React, { useRef, useEffect } from "react";
 import { Calendar } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import listPlugin from "@fullcalendar/list";
 import interactionPlugin from "@fullcalendar/interaction";
+
+const weekdays = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
+
+// TODO fetch events from API
 
 const FullCalendar = () => {
     const calendarRef = useRef<any>(null);
 
     useEffect(() => {
         const calendar = new Calendar(calendarRef.current, {
-            plugins: [dayGridPlugin, interactionPlugin],
+            plugins: [dayGridPlugin, listPlugin, interactionPlugin],
             initialView: "dayGridMonth",
             events: [
-                { title: "Event 1", date: "2024-05-23" },
-                { title: "Event 2", date: "2024-05-02" },
+                { title: "Event 1", date: "2024-05-17" },
+                { title: "Event 2", date: "2024-05-23" },
+                { title: "Event 3", date: "2024-05-01" },
+                { title: "Event 4", date: "2024-05-13" },
+                { title: "Event 5", date: "2024-05-30" },
             ],
+            height: "auto",
+            headerToolbar: {
+                start: "prev,next",
+                center: "title",
+                end: "dayGridMonth dayGridWeek listDay",
+            },
+            buttonText: {
+                month: "Mensal",
+                week: "Semanal",
+                day: "Diário",
+            },
+            locale: "pt-BR",
+            dayHeaders: false,
+            dayCellContent: (e) => {
+                return `${e.dayNumberText} - ${weekdays[e.date.getDay()]}`;
+            },
         });
 
         calendar.render();
