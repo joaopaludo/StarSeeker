4ba53c139d74c420041b6e47cfca136db610c562
joaopaludo <joaopgpaludo@gmail.com>
2024-07-03 22:34:26 -0300

    Artigo do projeto

--- app/__tests__/fase-lua.test.tsx
+++ app/__tests__/fase-lua.test.tsx
@@ -2,23 +2,32 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import FaseLua from "../home/fase-lua";
-
-Object.defineProperty(global.navigator, "geolocation", {
-    value: {
-        getCurrentPosition: jest.fn().mockImplementationOnce((success) =>
-            success({
-                coords: {
-                    latitude: 50,
-                    longitude: 50,
-                },
-            })
-        ),
-    },
-    writable: true,
-});
+import { act } from "react-dom/test-utils";
 
 describe("FaseLua component", () => {
-    it("prompts for location update when no location is set", () => {
+    beforeEach(() => {
+        // Mock API de geolocalização
+        Object.defineProperty(global.navigator, "geolocation", {
+            value: {
+                getCurrentPosition: jest
+                    .fn()
+                    .mockImplementationOnce((success) =>
+                        act(() => {
+                            success({
+                                coords: {
+                                    latitude: 43.65,
+                                    longitude: -79.38,
+                                },
+                            });
+                        })
+                    ),
+                watchPosition: jest.fn(),
+            },
+            writable: true,
+        });
+    });
+
+    it("Deve solicitar atualização de localização quando não está setada", () => {
         const queryClient = new QueryClient();
 
         render(
